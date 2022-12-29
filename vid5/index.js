bbfacd8358a6b50912941bb8ba1cbaf62ff713a0
Priyansh Nahar <priyanshnahar1998@gmail.com>
2022-12-29 10:01:50 +0530

    Practice

---
+++ vid5/index.js
@@ -0,0 +1,24 @@
+let a = "Priyansh"
+let b = 6
+console.log(a + b)
+console.log(typeof (a + b))
+
+const f = {
+    name: "Priyansh",
+    section: 44,
+    child: false,
+}
+f["marks"] = 69
+f["bestFrien"] = "noone"
+f["friend"] = "Deepak"
+console.log(f)
+
+
+const dictionary = {
+    excellent: "good",
+    intelligent: "sharp",
+    length: "lambai",
+    cup: "cupo",
+}
+
+console.log(Object.keys(dictionary));
