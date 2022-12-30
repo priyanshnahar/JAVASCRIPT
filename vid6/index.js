c20d16d4235cbff89ddff82a86e4cb1627c9b4dd
Priyansh Nahar <priyanshnahar1998@gmail.com>
2022-12-30 09:27:03 +0530

    Adding || vid 6

---
+++ vid6/index.js
@@ -0,0 +1,47 @@
+// operators in javascript
+// Arthmetic operators 
+
+// let a = 85;
+// let b = 56;
+
+
+// console.log("a + b =", a + b)
+// console.log("a ** b =", a ** b)
+// console.log("a * b =", a * b)
+// console.log("a++ =", a++)
+// console.log(a)
+// console.log("a++ =", a--)
+// console.log("++a =", ++a)
+// console.log("--a =", --a)
+// console.log("--a =", --a)
+
+// Assignment operators
+
+// let assignment = 1;
+// assignment += 5
+// console.log(assignment)
+
+
+
+// COMPARISON OPERATORS 
+// comparison operators are used when we need to compare two values or we have to return the result like the value is true or not 
+
+
+// let comp1 = 9 
+// let comp2 = 10
+
+// console.log("comp1 == comp2 is ", comp1 == comp2)
+// console.log("comp1 == comp2 is ", comp1 < comp2)
+
+
+// LOGICAL OPERATORS 
+// Logical operators are used to determine the logic between variables or values.   
+
+
+let x = 5;
+let y = 6;
+
+console.log(x<y && x==5)
+console.log(x<y || x==5)
+
+
