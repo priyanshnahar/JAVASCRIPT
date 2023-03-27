b3027a6c4395f23a7a8b8a3fad7c783167671a74
Priyansh Nahar <priyanshnahar1998@gmail.com>
2023-03-27 12:10:47 +0530

    Adding files

--- leetCode/twoSums.js
+++
@@ -1,7 +0,0 @@
-var twoSums = function(nums, target){
-        let map = new Map();
-        for(let i = 0; i < nums.length; i++) {
-            let num = nums[i];
-            let num2 = target- 
-        }
-}
